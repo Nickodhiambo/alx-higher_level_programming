9ae56e25e10049c0c48480d9097e65373d773d95
Nickodhiambo <nodhiambo01@gmail.com>
2023-06-06 13:05:29 -0700

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for dgt_1 in range(0, 10):
+    for dgt_2 in range(dgt_1 + 1, 10):
+        if dgt_1 == 8 and dgt_2 == 9:
+            print("{}{}".format(dgt_1, dgt_2))
+        else:
+            print("{}{}".format(dgt_1, dgt_2), end=", ")
