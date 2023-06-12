952c68b3fb1ff676d8e5a238d232af2b114a6dd4
Nickodhiambo <nodhiambo01@gmail.com>
2023-06-12 04:29:04 -0700

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers"""
+
+    for row in matrix:
+        for item in row:
+            print("{:d}".format(item), end=" " if item != row[-1] else "")
+        print()
