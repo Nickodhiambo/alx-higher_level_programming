6f4d5fa219ef07567e95776aaf2ac9f025d6707d
Nick <nodhiambo01@gmail.com>
2023-07-03 01:30:46 -0700

    Created a function that prints text delimited by .?:

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Supplies the function text_indentation"""
+
+
+def text_indentation(text):
+    """Prints text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    ch = 0
+    while ch < len(text) and text[ch] == ' ':
+        ch += 1
+
+    while ch < len(text):
+        print(text[ch], end="")
+        if text[ch] == "\n" or text[ch] in ".?:":
+            if text[ch] in ".?:":
+                print("\n")
+            ch += 1
+            while ch < len(text) and text[ch] == ' ':
+                ch += 1
+            continue
+        ch += 1
