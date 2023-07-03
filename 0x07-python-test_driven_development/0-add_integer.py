f81238361c996cf9d39bcf858a26bd8a65bc935f
Nick <nodhiambo01@gmail.com>
2023-07-03 01:25:46 -0700

    Created a function that adds two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module supplies one function, add_integer"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers"""
+
+    if (not isinstance(a, int) and
+            not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and
+            not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
