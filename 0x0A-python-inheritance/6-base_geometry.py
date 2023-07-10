5d57da23a2039859e65b69e217e0269e9768109d
Nick <nodhiambo01@gmail.com>
2023-07-10 08:00:49 -0700

    Defined the first public instance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the class BaseGeometry with one public instance method"""
+
+
+class BaseGeometry():
+    """Defines the "BaseGeometry class"""
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
