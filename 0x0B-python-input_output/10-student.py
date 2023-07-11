5a33d53355c5b71d51c470ac6a08fcd61d15a2fe
Nick <nodhiambo01@gmail.com>
2023-07-11 12:14:22 -0700

    Updated student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student():
+    """Models a class student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of student
+        if attrs is a list of strings, return only attr anmes contained
+        in the list
+        """
+        if (type(attrs) == list and
+                all(type(item) == str for item in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
