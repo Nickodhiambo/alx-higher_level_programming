3bc8ed871861ef4359d4b1a66edf13382a87b376
Nick <nodhiambo01@gmail.com>
2023-07-15 08:23:44 -0700

    Implemented __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,10 @@ class Rectangle(Base):
         for i in range(self.height):
             [print("#", end="") for j in range(self.width)]
             print("")
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                    self.x,
+                                                    self.y,
+                                                    self.width,
+                                                    self.height)
