d0578630a853772f1d389b30301e318ebd8ca5ec
Nick <nodhiambo01@gmail.com>
2023-07-23 23:26:35 -0700

    Added a class method that deserializes a json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,19 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes json string representation of json string object to a
+        file
+
+        Args:
+            list_objs(list): A list of instances that inherit from base
+        """
+        file_name = cls.__name__ + ".json"
+        with open(file_name, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_items = [item.to_dictionary() for item in list_objs]
+                f.write(Base.to_json_string(list_items))
