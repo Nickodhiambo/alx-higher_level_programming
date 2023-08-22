ccf63200de9ab3f598352fb858cb73f117e800fa
Nick <nodhiambo01@gmail.com>
2023-08-22 00:29:14 -0700

    update

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Creates a table called 'force_name' on MYSQL server
--- Vlaue to table attribute 'name' cannot be NULL
-CREATE TABLE IF NOT EXISTS `force_table`(`id` INT, 
+-- Value to table attribute 'name' cannot be NULL
+CREATE TABLE IF NOT EXISTS `force_name`(`id` INT, 
 					`name` VARCHAR(256) NOT NULL);
