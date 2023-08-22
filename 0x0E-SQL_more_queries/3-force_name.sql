14ad957bfef89faa7c6a3a1917c7474706549e03
Nick <nodhiambo01@gmail.com>
2023-08-22 00:20:18 -0700

    Implemented 'NOT NULL' constraint on a table

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- Creates a table called 'force_name' on MYSQL server
+-- Vlaue to table attribute 'name' cannot be NULL
+CREATE TABLE IF NOT EXISTS `force_table`(`id` INT, 
+					`name` VARCHAR(256) NOT NULL);
