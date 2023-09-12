aaf405fb5b20829513e45600f8e6799d4747601d
Nick <nodhiambo01@gmail.com>
2023-09-12 07:34:35 -0700

    Created a square subclass

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+const squarePrev = require('./5-square');
+
+module.exports = class Square extends squarePrev {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+};
