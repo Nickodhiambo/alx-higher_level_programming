7dec72f08004d205452985fbef173c70b7123af3
Nick <nodhiambo01@gmail.com>
2023-09-18 00:32:48 -0700

    Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This script connects a python script to a db using SQLAlchemy ORM"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(argv[1], argv[2], argv[3]),
+            pool_pre_ping=True
+            )
+
+    my_session_maker = sessionmaker(bind=engine)
+    my_session = my_session_maker()
+
+    for state in my_session.query(State):
+        if argv[4] == state.name:
+            print("{}".format(state.id))
+            break
+    else:
+        print("Not found")
+
+    my_session.close()
